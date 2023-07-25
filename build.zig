fe08aaa026b65c644c79ee85baa2e4562888772c
Michal Ziulek <michalziulek@gmail.com>
2023-07-25 15:08:13 +0200

    -

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const lib = b.addStaticLibrary(.{
         .name = "dawn",
-        .target = .{ .cpu = .x86_64, .os = .windows, .abi = .gnu },
+        .target = .{ .cpu_arch = .x86_64, .os_tag = .windows, .abi = .gnu },
         .optimize = .ReleaseFast,
     });
     lib.linkSystemLibraryName("dawn");
