20f68507f7f7021b459a7d7e2896f070fdc669fd
Michal Ziulek <michalziulek@gmail.com>
2023-07-25 21:41:59 +0200

    -

--- build.zig
+++ build.zig
@@ -1,10 +1,3 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    b.installArtifact(b.addStaticLibrary(.{
-        .name = "dawn",
-        .root_source_file = .{ .path = "dawn.lib" },
-        .target = .{ .cpu_arch = .x86_64, .os_tag = .windows, .abi = .gnu },
-        .optimize = .ReleaseFast,
-    }));
-}
+pub fn build(_: *std.Build) void {}
