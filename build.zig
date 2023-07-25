d2d35a707bf916fa2625029a32d8df024ef8e947
Michal Ziulek <michalziulek@gmail.com>
2023-07-25 16:18:37 +0200

    -

--- build.zig
+++ build.zig
@@ -3,8 +3,14 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const lib = b.addStaticLibrary(.{
         .name = "dawn",
+        .root_source_file = .{ .path = "dawn.lib" },
         .target = .{ .cpu_arch = .x86_64, .os_tag = .windows, .abi = .gnu },
         .optimize = .ReleaseFast,
     });
-    lib.linkSystemLibraryName("dawn");
+    b.installArtifact(lib);
+
+    //for (b.install_tls.step.dependencies.items) |dep_step| {
+    //    const inst = dep_step.cast(std.Build.Step.InstallArtifact) orelse continue;
+    //    std.debug.print("{s}\n", .{inst.artifact.name});
+    //}
 }
